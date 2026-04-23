a1d4921fd3bdabe77632fb7e704972de24ff5157
Praveen Kumar <praveenkumar75566@gmail.com>
2026-04-23 11:18:01 +0530

    Added string decalration

--- SyntaxAndVariable.js
+++ SyntaxAndVariable.js
@@ -1,3 +1,12 @@
-let a = 10;
-let b = 20;
-console.log(a + b);
+let x = 10; // let can be redeclared
+var y = 20; // var can be redeclared
+const z = 30; // const can not be redeclared
+const country = "India";
+//this is a single line comment
+/* 
+this 
+is a 
+multi-line 
+comment
+*/
+console.log(x + y + z + " " + country);
