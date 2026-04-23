192e746654f6a0d5559f021faa17bcc8a3c884b9
Praveen Kumar <praveenkumar75566@gmail.com>
2026-04-23 18:34:12 +0530

    While loop

---
+++ ControlStructure.js
@@ -0,0 +1,39 @@
+// let age = 170
+
+
+// if (isNaN(age)) {
+//     console.log("Please enter a valid number");
+// }
+// else if (age > 90) {
+//     console.log("Enter age below 90");
+// }
+// else if (age >= 18) {
+//     console.log("You are eligible to vote");
+// }
+
+// else {
+//     console.log("Not eligible");
+// }
+
+
+// let day = 2;
+// switch (day) {
+//     case 1:
+//         console.log("Monday");
+//         break;
+//     case 2:
+//         console.log("Tuesday");
+//         break;
+//     case 3:
+//         console.log("Webnesday");
+//         break;
+//     default:
+//         console.log("Other");
+
+// }
+
+
+for (let i = 0; i <= 10; i++) {
+    console.log(`Counr: ${i}`);
+
+}
