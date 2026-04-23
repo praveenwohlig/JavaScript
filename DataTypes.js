1cc0f34af7fe6f1e12c8efb537908b4e7f2ae772
Praveen Kumar <praveenkumar75566@gmail.com>
2026-04-23 14:28:09 +0530

    New changes

--- DataTypes.js
+++ DataTypes.js
@@ -1,37 +1,46 @@
-//String
-let name1 = "John";
-//Number
-let age = 20;
-let percentage = 90.5;
-//Boolean
-let isStudent = true;
-//Null
-let address = null;
-//Undefined
-let city;
+// //String
+// let name1 = "John";
+// //Number
+// let age = 20;
+// let percentage = 90.5;
+// //Boolean
+// let isStudent = true;
+// //Null
+// let address = null;
+// //Undefined
+// let city;
 
-let myObj = {
-    name: "John",
-    age: 20,
-    isStudent: true,
-    address: null,
-    city: undefined
-};
+// let myObj = {
+//     name: "John",
+//     age: 20,
+//     isStudent: true,
+//     address: null,
+//     city: undefined
+// };
 
-let x = Symbol("name");
-let y = Symbol("name");
-console.log(x == y);
+// let x = Symbol("name");
+// let y = Symbol("name");
+// console.log(x == y); //Symbol are unique always
 
-console.log("Name : " + name1);
-console.log("Age : " + age);
-console.log("Is Student : " + isStudent);
-console.log("Address : " + address);
-console.log("City : " + city);
+// myObj.name = "Ajith"
+// console.log(myObj.name);
+// console.log(myObj)
 
-console.log(typeof name1);
-console.log(typeof age);
-console.log(typeof isStudent);
-console.log(typeof address);
-console.log(typeof city);
-console.log(typeof myObj);
-console.log(myObj.name);
+// console.log("Name : " + name1);
+// console.log("Age : " + age);
+// console.log("Is Student : " + isStudent);
+// console.log("Address : " + address);
+// console.log("City : " + city);
+
+// console.log(typeof name1);
+// console.log(typeof age);
+// console.log(typeof isStudent);
+// console.log(typeof address);
+// console.log(typeof city);
+// console.log(typeof myObj);
+// console.log(myObj.name);
+
+var add = 10
+add++
+console.log(add);
+
